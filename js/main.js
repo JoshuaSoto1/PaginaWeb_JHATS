24174f3795f7607a5bfbb707127dc3945605596d
Joshua Soto Hernandez <joshdz1605@gmail.com>
2026-06-23 22:43:09 -0600

    VERSION DE PRODUCCIÓN 1

--- js/main.js
+++ js/main.js
@@ -9,38 +9,38 @@ const marcas = {
       { id: 'B1', nombre: 'EL BARBAS HATS X CT - CHROME I (LAST EDITION) ', precio: '₡25.000', foto: 'assets/images/gorras/B1D.webp', fotos: ['assets/images/gorras/B1D.webp','assets/images/gorras/B3IZ.webp','assets/images/gorras/B2L.webp'] },
       { id: 'B2', nombre: 'BARBAS PREMIUM 2.0 — DARK NEBULA', precio: '₡25.000', foto: 'assets/images/gorras/B2FPPPP.webp', fotos: ['assets/images/gorras/B2FPPPP.webp','assets/images/gorras/B2LI.webp','assets/images/gorras/B2LD.webp'] },
       { id: 'B3', nombre: 'BARBAS HATS - HEARTBREAK WILITO', precio: '₡25.000', foto: 'assets/images/gorras/B3FFF.webp', fotos: ['assets/images/gorras/B3FFF.webp','assets/images/gorras/B3L.webp','assets/images/gorras/B3A.webp'] },
-      { id: 'B4', nombre: 'Barbas B4', precio: '₡25.000', foto: 'assets/images/gorras/barbas/B4/portada.jpg', fotos: ['assets/images/gorras/barbas/B4/foto1.jpg','assets/images/gorras/barbas/B4/foto2.jpg','assets/images/gorras/barbas/B4/foto3.jpg'] },
-      { id: 'B5', nombre: 'Barbas B5', precio: '₡25.000', foto: 'assets/images/gorras/barbas/B5/portada.jpg', fotos: ['assets/images/gorras/barbas/B5/foto1.jpg','assets/images/gorras/barbas/B5/foto2.jpg','assets/images/gorras/barbas/B5/foto3.jpg'] },
-      { id: 'B6', nombre: 'Barbas B6', precio: '₡25.000', foto: 'assets/images/gorras/barbas/B6/portada.jpg', fotos: ['assets/images/gorras/barbas/B6/foto1.jpg','assets/images/gorras/barbas/B6/foto2.jpg','assets/images/gorras/barbas/B6/foto3.jpg'] },
+      { id: 'B4', nombre: 'EL BARBAS HATS X CT - ROCKSTAR I CT', precio: '₡25.000', foto: 'assets/images/gorras/B4F.webp', fotos: ['assets/images/gorras/B4F.webp','assets/images/gorras/B4L.webp','assets/images/gorras/B4LD.webp'] },
+      { id: 'B5', nombre: 'BARBAS HATS GORRA - CHROME III EDICIÓN', precio: '₡25.000', foto: 'assets/images/gorras/B5F.webp', fotos: ['assets/images/gorras/B5F.webp','assets/images/gorras/B5L.webp','assets/images/gorras/B5LI.webp'] },
+      { id: 'B6', nombre: 'BARBAS HATS - THE BLACK 77', precio: '₡25.000', foto: 'assets/images/gorras/B6F.webp', fotos: ['assets/images/gorras/B6F.webp','assets/images/gorras/B6L.webp','assets/images/gorras/B6A.webp'] },
     ]
   },
   'Dandy': {
     modelos: [
-      { id: 'D1', nombre: 'Dandy D1', precio: '₡25.000', foto: 'assets/images/gorras/dandy/D1/portada.jpg', fotos: ['assets/images/gorras/dandy/D1/foto1.jpg','assets/images/gorras/dandy/D1/foto2.jpg','assets/images/gorras/dandy/D1/foto3.jpg'] },
-      { id: 'D2', nombre: 'Dandy D2', precio: '₡25.000', foto: 'assets/images/gorras/dandy/D2/portada.jpg', fotos: ['assets/images/gorras/dandy/D2/foto1.jpg','assets/images/gorras/dandy/D2/foto2.jpg','assets/images/gorras/dandy/D2/foto3.jpg'] },
-      { id: 'D3', nombre: 'Dandy D3', precio: '₡25.000', foto: 'assets/images/gorras/dandy/D3/portada.jpg', fotos: ['assets/images/gorras/dandy/D3/foto1.jpg','assets/images/gorras/dandy/D3/foto2.jpg','assets/images/gorras/dandy/D3/foto3.jpg'] },
+      { id: 'D1', nombre: 'DANDY HATS - DANDYWOOD PURP', precio: '₡25.000', foto: 'assets/images/gorras/D1F.webp', fotos: ['assets/images/gorras/D2.webp','assets/images/gorras/D3.webp','assets/images/gorras/D4.webp'] },
+      { id: 'D2', nombre: 'GORRA DANDY HATS - CLAVE ALI ROSADA', precio: '₡25.000', foto: 'assets/images/gorras/D2F.webp', fotos: ['assets/images/gorras/D2F.webp','assets/images/gorras/D3F.webp','assets/images/gorras/D4A.webp'] },
+      { id: 'D3', nombre: 'DANDY HATS X TOMBOCHIO — FOR ALL OUR HATERS', precio: '₡25.000', foto: 'assets/images/gorras/D3D.webp', fotos: ['assets/images/gorras/D3D.webp','assets/images/gorras/D3L.webp','assets/images/gorras/D3A.webp'] },
     ]
   },
   '31 Hats': {
     modelos: [
-      { id: '31-1', nombre: '31 Hats M1', precio: '₡25.000', foto: 'assets/images/gorras/31hats/M1/portada.jpg', fotos: ['assets/images/gorras/31hats/M1/foto1.jpg','assets/images/gorras/31hats/M1/foto2.jpg','assets/images/gorras/31hats/M1/foto3.jpg'] },
-      { id: '31-2', nombre: '31 Hats M2', precio: '₡25.000', foto: 'assets/images/gorras/31hats/M2/portada.jpg', fotos: ['assets/images/gorras/31hats/M2/foto1.jpg','assets/images/gorras/31hats/M2/foto2.jpg','assets/images/gorras/31hats/M2/foto3.jpg'] },
-      { id: '31-3', nombre: '31 Hats M3', precio: '₡25.000', foto: 'assets/images/gorras/31hats/M3/portada.jpg', fotos: ['assets/images/gorras/31hats/M3/foto1.jpg','assets/images/gorras/31hats/M3/foto2.jpg','assets/images/gorras/31hats/M3/foto3.jpg'] },
-      { id: '31-4', nombre: '31 Hats M4', precio: '₡25.000', foto: 'assets/images/gorras/31hats/M4/portada.jpg', fotos: ['assets/images/gorras/31hats/M4/foto1.jpg','assets/images/gorras/31hats/M4/foto2.jpg','assets/images/gorras/31hats/M4/foto3.jpg'] },
-      { id: '31-5', nombre: '31 Hats M5', precio: '₡25.000', foto: 'assets/images/gorras/31hats/M5/portada.jpg', fotos: ['assets/images/gorras/31hats/M5/foto1.jpg','assets/images/gorras/31hats/M5/foto2.jpg','assets/images/gorras/31hats/M5/foto3.jpg'] },
-      { id: '31-6', nombre: '31 Hats M6', precio: '₡25.000', foto: 'assets/images/gorras/31hats/M6/portada.jpg', fotos: ['assets/images/gorras/31hats/M6/foto1.jpg','assets/images/gorras/31hats/M6/foto2.jpg','assets/images/gorras/31hats/M6/foto3.jpg'] },
+      { id: '31-1', nombre: 'GORRA 31 HATS - EL MAGO TOTAL BLACK', precio: '₡25.000', foto: 'assets/images/gorras/M1F.webp', fotos: ['assets/images/gorras/M1F.webp','assets/images/gorras/M1L.webp','assets/images/gorras/M1A.png'] },
+      { id: '31-2', nombre: 'GORRA 31 - HATS BACKPACK BOYZ ROJA', precio: '₡25.000', foto: 'assets/images/gorras/M2F.webp', fotos: ['assets/images/gorras/M2F.webp','assets/images/gorras/M2L.webp','assets/images/gorras/M2A.webp'] },
+      { id: '31-3', nombre: '31 HATS X EL MAGO — M ROJA', precio: '₡25.000', foto: 'assets/images/gorras/M3F.webp', fotos: ['assets/images/gorras/M3LI.webp','assets/images/gorras/M3L.webp','assets/images/gorras/M3A.webp'] },
+      { id: '31-4', nombre: 'GORRA 31 HATS - TOTAL BLACK NY FLAMES', precio: '₡25.000', foto: 'assets/images/gorras/M4F.webp', fotos: ['assets/images/gorras/M4F.webp','assets/images/gorras/M4L.webp','assets/images/gorras/M4A.webp'] },
+      { id: '31-5', nombre: 'GORRA 31 HATS - EL MAGO FOREVER WINGS', precio: '₡25.000', foto: 'assets/images/gorras/M5F.webp', fotos: ['assets/images/gorras/M5L.webp','assets/images/gorras/M5LI.webp','assets/images/gorras/M5A.webp'] },
+      { id: '31-6', nombre: 'GORRA 31 HATS - PINK RECON', precio: '₡25.000', foto: 'assets/images/gorras/M6F.webp', fotos: ['assets/images/gorras/M6L.webp','assets/images/gorras/M6LI.webp','assets/images/gorras/M6A.webp'] },
     ]
   },
   'Otras Marcas': {
     modelos: [
-      { id: 'O1', nombre: 'Otras O1', precio: '₡25.000', foto: 'assets/images/gorras/otras/O1/portada.jpg', fotos: ['assets/images/gorras/otras/O1/foto1.jpg','assets/images/gorras/otras/O1/foto2.jpg','assets/images/gorras/otras/O1/foto3.jpg'] },
-      { id: 'O2', nombre: 'Otras O2', precio: '₡25.000', foto: 'assets/images/gorras/otras/O2/portada.jpg', fotos: ['assets/images/gorras/otras/O2/foto1.jpg','assets/images/gorras/otras/O2/foto2.jpg','assets/images/gorras/otras/O2/foto3.jpg'] },
-      { id: 'O3', nombre: 'Otras O3', precio: '₡25.000', foto: 'assets/images/gorras/otras/O3/portada.jpg', fotos: ['assets/images/gorras/otras/O3/foto1.jpg','assets/images/gorras/otras/O3/foto2.jpg','assets/images/gorras/otras/O3/foto3.jpg'] },
-      { id: 'O4', nombre: 'Otras O4', precio: '₡25.000', foto: 'assets/images/gorras/otras/O4/portada.jpg', fotos: ['assets/images/gorras/otras/O4/foto1.jpg','assets/images/gorras/otras/O4/foto2.jpg','assets/images/gorras/otras/O4/foto3.jpg'] },
-      { id: 'O5', nombre: 'Otras O5', precio: '₡25.000', foto: 'assets/images/gorras/otras/O5/portada.jpg', fotos: ['assets/images/gorras/otras/O5/foto1.jpg','assets/images/gorras/otras/O5/foto2.jpg','assets/images/gorras/otras/O5/foto3.jpg'] },
-      { id: 'O6', nombre: 'Otras O6', precio: '₡25.000', foto: 'assets/images/gorras/otras/O6/portada.jpg', fotos: ['assets/images/gorras/otras/O6/foto1.jpg','assets/images/gorras/otras/O6/foto2.jpg','assets/images/gorras/otras/O6/foto3.jpg'] },
-      { id: 'O7', nombre: 'Otras O7', precio: '₡25.000', foto: 'assets/images/gorras/otras/O7/portada.jpg', fotos: ['assets/images/gorras/otras/O7/foto1.jpg','assets/images/gorras/otras/O7/foto2.jpg','assets/images/gorras/otras/O7/foto3.jpg'] },
-      { id: 'O8', nombre: 'Otras O8', precio: '₡25.000', foto: 'assets/images/gorras/otras/O8/portada.jpg', fotos: ['assets/images/gorras/otras/O8/foto1.jpg','assets/images/gorras/otras/O8/foto2.jpg','assets/images/gorras/otras/O8/foto3.jpg'] },
+      { id: 'O1', nombre: 'Otras O1', precio: '₡25.000', foto: 'assets/images/gorras/O1F.webp', fotos: ['assets/images/gorras/O1L.webp','assets/images/gorras/O1LI.webp','assets/images/gorras/O1AB.webp'] },
+      { id: 'O2', nombre: 'Otras O2', precio: '₡25.000', foto: 'assets/images/gorras/O2F.webp', fotos: ['assets/images/gorras/O2L.webp','assets/images/gorras/O2LI.webp','assets/images/gorras/O2A.webp'] },
+      { id: 'O3', nombre: 'Otras O3', precio: '₡25.000', foto: 'assets/images/gorras/O3F.webp', fotos: ['assets/images/gorras/O3F.webp','assets/images/gorras/O3L.webp','assets/images/gorras/O3LI.webp'] },
+      { id: 'O4', nombre: 'Otras O4', precio: '₡25.000', foto: 'assets/images/gorras/O4F.webp', fotos: ['assets/images/gorras/O4L.webp','assets/images/gorras/O4LI.webp','assets/images/gorras/O4A.webp'] },
+      { id: 'O5', nombre: 'Otras O5', precio: '₡25.000', foto: 'assets/images/gorras/O5F.webp', fotos: ['assets/images/gorras/O5F.webp','assets/images/gorras/O5L.webp','assets/images/gorras/O5A.webp'] },
+      { id: 'O6', nombre: 'Otras O6', precio: '₡25.000', foto: 'assets/images/gorras/O6F.webp', fotos: ['assets/images/gorras/O6F.webp','assets/images/gorras/O6L.webp','assets/images/gorras/O6LI.webp'] },
+      { id: 'O7', nombre: 'Otras O7', precio: '₡25.000', foto: 'assets/images/gorras/07F.webp', fotos: ['assets/images/gorras/07F.webp','assets/images/gorras/07L.webp','assets/images/gorras/07LD.webp'] },
+      { id: 'O8', nombre: 'Otras O8', precio: '₡25.000', foto: 'assets/images/gorras/08F.webp', fotos: ['assets/images/gorras/08FF.webp','assets/images/gorras/08LD.webp','assets/images/gorras/08LI.webp'] },
     ]
   }
 };
@@ -305,7 +305,7 @@ function fixIOSVideo() {
 function initMusica() {
   const audio = new Audio('assets/audio/cancion.mp3');
   audio.loop = true;
-  audio.volume = 0.10;
+  audio.volume = 0.1;
 
   const iniciarMusica = () => {
     audio.play().catch(() => {});
